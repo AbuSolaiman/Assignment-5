856e0a7f7ad24d143a8361df602b96128661e368
AbuSolaiman <md.abusolaiman98@gmail.com>
2025-08-29 00:09:29 +0600

    added love system

--- emergency.js
+++ emergency.js
@@ -3,3 +3,13 @@
  menuToggle.addEventListener("click", () => {
      menuItems.classList.toggle("hidden")
  });
+
+ const heartBtn = document.getElementById("heartBtn");
+ let heartBtnValue = parseInt(heartBtn.innerText);
+ const likeCount = document.getElementsByClassName("likeCount");
+ for (let i = 0; i < likeCount.length; i++) {
+     likeCount[i].addEventListener("click", () => {
+         heartBtnValue++;
+         heartBtn.innerText = heartBtnValue;
+     });
+ };
