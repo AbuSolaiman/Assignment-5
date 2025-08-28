739e1891e9eb4826f8df94e91b0bf7f6b6d719fe
AbuSolaiman <md.abusolaiman98@gmail.com>
2025-08-29 00:07:05 +0600

    added menu toggle

---
+++ emergency.js
@@ -0,0 +1,5 @@
+ const menuToggle = document.getElementById("menuToggle");
+ const menuItems = document.getElementById("menuItems");
+ menuToggle.addEventListener("click", () => {
+     menuItems.classList.toggle("hidden")
+ });
