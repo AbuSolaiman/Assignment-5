0877fcbaeb6323ea87d652d7d953a4b82730040d
AbuSolaiman <md.abusolaiman98@gmail.com>
2025-08-29 09:57:18 +0600

    change and added something in emergency.js code

--- emergency.js
+++ emergency.js
@@ -1,29 +1,38 @@
+
  const menuToggle = document.getElementById("menuToggle");
  const menuItems = document.getElementById("menuItems");
+
  menuToggle.addEventListener("click", () => {
      menuItems.classList.toggle("hidden")
  });
 
+
+
  const heartBtn = document.getElementById("heartBtn");
  let heartBtnValue = parseInt(heartBtn.innerText);
  const likeCount = document.getElementsByClassName("likeCount");
+
+
  for (let i = 0; i < likeCount.length; i++) {
      likeCount[i].addEventListener("click", () => {
          heartBtnValue++;
          heartBtn.innerText = heartBtnValue;
      });
- };
+    }
 
- let coin = 100;
+let coin = 100;
 const coinCount = document.getElementById("coinCount");
 const callHistoryItems = document.getElementById("callHistoryItems");
+
  function updateCoinCount(serviceName, serviceNumber) {
      if (coin < 20) {
          alert("❌ আপনার কাছে পর্যাপ্ত কয়েন নেই! কল করতে কমপক্ষে ২০ কয়েন প্রয়োজন।");
      } else {
          alert(`📞Calling ${serviceName} ${serviceNumber}...`);
+
          coin -= 20;
          coinCount.innerText = coin;
+
          const div = document.createElement("div");
          div.innerHTML = `
      <div class="bg-white p-5 rounded-lg shadow-md mt-5 flex justify-between items-center">
@@ -39,10 +48,12 @@ const callHistoryItems = document.getElementById("callHistoryItems");
          callHistoryItems.appendChild(div);
      }
  };
+
  document.getElementById("clearHistoryBtn").addEventListener("click", () => {
      callHistoryItems.innerHTML = "";
  });
 
+ 
  let copyCount = 0;
  const copyCountElement = document.getElementById("copyCount");
 
